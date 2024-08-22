10342a43df8a86986729aea06f4825e7b168915b
Rv waqas <145973183+Rvwaqas@users.noreply.github.com>
2024-08-22 22:43:38 +0500

    Add files via upload
    Add functionall file

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer"
+
+let result= await inquirer.prompt({
+    name:"as",
+    type:"confirm",
+    message:"Check more",
+    default:true
+})
+
+async function calcultor(val:boolean){
+
+    if(val){
+let weight=await inquirer.prompt({
+    name:"weights",
+    type:"number",
+    message:"Enter your weight"
+
+}); 
+
+let height= await inquirer.prompt({
+    name:"heights",
+    type:"number",
+    message:"Enter your height"
+});
+
+
+
+let BMI= weight.weights/height.heights**2;
+console.log(BMI)
+    }
+}
+
+calcultor(result.as)
+
+
+
+
+
+
